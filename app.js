ecf4be611a3dc68c0b68805f82ed7b999a96e2b9
Kum Jude Tem <judekum14@gmail.com>
2020-09-08 20:10:41 +0100

    added progress bar functionality

--- app.js
+++ app.js
@@ -7,22 +7,28 @@ const next1 = document.getElementById("next-1");
 const back = document.getElementById("back");
 const back1 = document.getElementById("back-1");
 
+const progress = document.getElementById("progress");
+
 next.onclick = () => {
   form1.style.left = "-450px";
   form2.style.left = "40px";
+  progress.style.width = "240px";
 };
 
 back.onclick = () => {
   form1.style.left = "40px";
-  form2.style.left = "450px";
+    form2.style.left = "450px";
+    progress.style.width = "120px";
 };
 
 next1.onclick = () => {
   form2.style.left = "-450px";
-  form3.style.left = "40px";
+    form3.style.left = "40px";
+    progress.style.width = "360px";
 };
 
 back1.onclick = () => {
   form2.style.left = "40px";
-  form3.style.left = "450px";
+    form3.style.left = "450px";
+    progress.style.width = "240px";
 };
